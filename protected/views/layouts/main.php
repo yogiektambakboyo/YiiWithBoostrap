d3551ed65e895d5392df0cc351c7ec0c93cdd7a6
Yogi Aditya <yogiadityapurnomo@gmail.com>
2013-07-26 16:01:09 +0700

    master

---
+++ protected/views/layouts/main.php
@@ -0,0 +1,95 @@
+<?php /* @var $this Controller */ ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
+
+
+
+    <!-- blueprint CSS framework -->
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
+	<![endif]-->
+
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+
+
+
+
+	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+    <?php Yii::app()->bootstrap->register() ?>
+</head>
+
+<body>
+
+<div class="container" id="page">
+
+	<div id="header">
+		<div id="logo">
+            <?php $this->widget('bootstrap.widgets.TbNavbar', array(
+                'type'=>'inverse', // null or 'inverse'
+                'brand'=>'Project name',
+                'brandUrl'=>'#',
+                'collapse'=>true, // requires bootstrap-responsive.css
+                'items'=>array(
+                    array(
+                        'class'=>'bootstrap.widgets.TbMenu',
+                        'items'=>array(
+                            array('label'=>'Home', 'url'=>array('/site/index'), 'icon'=>'home'),
+                            array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+                            array('label'=>'Contact', 'url'=>array('/site/contact')),
+                            array('label'=>'Gii', 'url'=>'index.php?r=gii'),
+                            array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+                            array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+                            array('label'=>'Dropdown', 'url'=>'#', 'items'=>array(
+                                array('label'=>'Action', 'url'=>'#'),
+                                array('label'=>'Another action', 'url'=>'#'),
+                                array('label'=>'Something else here', 'url'=>'#'),
+                                '---',
+                                array('label'=>'NAV HEADER'),
+                                array('label'=>'Separated link', 'url'=>'#'),
+                                array('label'=>'One more separated link', 'url'=>'#'),
+                            )),
+                        ),
+                    ),
+                    '<form class="navbar-search pull-left" action=""><input type="text" class="search-query span2" placeholder="Search"></form>',
+
+                ),
+            )); ?>
+		</div>
+
+	</div><!-- header -->
+
+	<div id="mainmenu">
+
+
+	</div><!-- mainmenu -->
+
+    <div class="clear"></div>
+
+	<?php if(isset($this->breadcrumbs)):?>
+		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
+			'links'=>$this->breadcrumbs,
+		)); ?><!-- breadcrumbs -->
+	<?php endif?>
+
+	<?php echo $content; ?>
+
+	<div class="clear"></div>
+
+
+
+</div><!-- page -->
+
+<div id="footer">
+    Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
+    All Rights Reserved.<br/>
+    <?php echo Yii::powered(); ?>
+</div><!-- footer -->
+
+</body>
+</html>
